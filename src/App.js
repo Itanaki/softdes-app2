f247fccb645d7cbfa0e85691a6161d7cffcd813c
Itanaki <qedbpadual@tip.edu.ph>
2024-12-04 23:56:57 +0800

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-  // src/App.js
-  import React from 'react';
-  import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-  import LandingPage from './components/LandingPage/LandingPage.js';
-  import MonitoringPage from './components/MonitoringPage/MonitoringPage.js';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage.js';
+import MonitoringPage from './components/MonitoringPage/MonitoringPage.js';
 
-  const App = () => {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/HOME" element={<LandingPage />} />         {/* Landing Page Route */}
-          <Route path="/monitoring" element={<MonitoringPage />} /> {/* Monitoring Page Route */}
-        </Routes>
-      </Router>
-    );
-  };
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/HOME" element={<LandingPage />} />         {/* Landing Page Route */}
+        <Route path="/monitoring" element={<MonitoringPage />} /> {/* Monitoring Page Route */}
+      </Routes>
+    </Router>
+  );
+};
 
-  export default App;
+export default App;
